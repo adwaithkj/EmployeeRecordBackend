2eb622d4616e82ffb18737fb2ee6c1d706baac59
Adwaith <adwaithkj@gmail.com>
2022-07-04 00:21:24 +0530

    added the user id role id relation table

--- src/main/java/com/oracle/employeerecord/model/Employee.java
+++ src/main/java/com/oracle/employeerecord/model/Employee.java
@@ -1,11 +1,20 @@
 package com.oracle.employeerecord.model;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumns;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.*;
 
 @Entity
 public class Employee {
@@ -22,6 +31,10 @@ public class Employee {
     @NotBlank
     @Size(max = 20)
     private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 
     public Long getId() {
         return id;
